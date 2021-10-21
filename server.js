94e26875b43f1d2bfcc69accb36f76e3abeaa343
david-allison12 <dgreenal@gmail.com>
2021-10-20 21:47:01 -0400

    Connected to localhost mongodb | Added module.exports

---
+++ server.js
@@ -0,0 +1,54 @@
+/* == External Modules === */
+const express = require('express')
+
+/* == Internal Modules === */
+
+
+
+/* == Cors Modules === */
+const cors = require('cors')
+
+/* PORT */
+const PORT = process.env.PORT || 3003
+
+
+/* == Express Instance === */
+const app = express()
+
+
+
+/* == DB Connection  === */
+require('./config/db.connection')
+
+
+
+/* == middlewares  === */
+
+// Set up Cors middleware
+const whitelist = ['http://localhost:3000', 'heroku frontend url here']
+const coreOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(coreOptions))
+app.use(express.json())
+
+
+
+/* == Routes === */
+app.get('/', (req, res) => {
+    res.send('This is the working route.')
+})
+
+
+/* == Server Bind  === */
+
+app.listen(PORT, (req, res) => {
+    console.log(`Now listening on PORT ${PORT}🥳`)
+})
